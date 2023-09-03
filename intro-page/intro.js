87dc0defcfa113f2209da855a4af47d11af859fe
Ethan <111640712+pighoui@users.noreply.github.com>
2023-09-04 09:59:59 +1000

    Implement new story and replace typing text with printing line by line
    The new story fits better with the UI of the game, being more light-hearted.
    
    Instead of a typing text effect, each line of the story is printed upon keydown.

--- intro-page/intro.js
+++ intro-page/intro.js
@@ -1,30 +1,45 @@
-function typeWriter() {
+const txt = `On one sunny summer day, you're just chilling with your mates Nadal, Federer and Djokovic around the courts of Wimbledon.
+            Up come three men, the spitting image of Nadal, Federer and Djokovic.
+            They call themselves Lord Nadalmort, Darth Fader and the Joker (Originality™).
+            They also claim to be able to take a game of tennis off your mates.
+            "HA!!!" laugh your three mates at the exact same time.
+            "HA!!!" laugh Lord Nadalmort, Darth Fader and the Joker in return.
+            Before the egotistical maniacs come to blows, you suggest that the six of them face off in a game of 3v3 tennis.
+            A date and time are agreed.
+            In the week leading up to the sell-out event, your mates are no longer so sure about their chances.
+            The giants of tennis have come to the realisation that they also have giant heads! They have no clue how to work together.
+            As such, they have asked for your help to coordinate them during the match. `
 
-    document.getElementById("text").innerHTML = ""; // Clear the page
-    
-    // Newline
-    if (txt.charAt(i) == '.') {
-        document.getElementById("text").innerHTML += ".</br>"; // Print a '.' followed by a newline
-        i++; // Skips the '.' being printed again
+
+// Counter for current place in txt
+let txtIndex = 0;
+
+// Returns the next line in txt
+function nextLine() {
+    for (let i = txtIndex; i < txt.length; i++) {
+        if (txt.charAt(i) == '.') {
+            let line = txt.slice(txtIndex, i + 1);
+            txtIndex = i + 1; // Set txtIndex to start of next line
+            return line;
+        }
     }
-
-    // New paragraph
-    if (txt.slice(i-3, i) == '………') {
-        document.getElementById("text").innerHTML = ""; // Clear the text on page
-    }
-
-    if (i < txt.length) { 
-        document.getElementById("text").innerHTML += txt.charAt(i);
-        i++;
-        setTimeout(typeWriter, speed); // After the speed expires, run typeWriter() again
-    } 
 }
 
-// Counter variable for current place in text
-let i = 0;
+// Clears the story, asking user if they want to start the game
+function prompt() {
+    document.getElementById("text").innerHTML = "";
+    
+}
 
-// The duration of each character in ms
-let speed = 70;
+window.addEventListener("keydown", (event) => {
+    
+    if (txtIndex == txt.length - 1) {
+        // Reached the end of txt
+        prompt()
+    } else {
+        document.getElementById("text").innerHTML += `${nextLine()}</br>`;
+        document.querySelector("h3").innerHTML = "Press any key to continue.";
+    }
+    
+})
 
-// Text
-const txt = `Demonic humanoids are taking over planet Earth. Mankind is proving to be no match against their advance. But not all hope is lost……… The humanoids have challenged humanity to a three-person tennis duel. Should we win, our planet would be saved. Loss does not bear thinking of……… The Big Three have united once more to save mankind. But they cannot do it alone. Only you have the tennis mind to coordinate them. Only you can lead the charge to reclaim our planet.`
